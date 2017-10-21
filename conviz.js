04ec186ee4a90150fc5052d813f305e26b720046
ronrest <ronny.coding@gmail.com>
2017-10-21 21:38:22 +1100

    FEAT: Add Vertex

--- conviz.js
+++ conviz.js
@@ -1,2 +1,12 @@
 // Based on code from https://www.sitepoint.com/building-3d-engine-javascript/
 
+// #############################################################################
+//                                 PRIMITIVES
+// #############################################################################
+var Vertex = function(x, y, z) {
+    // Defines a vertex in 3D space for our 3D objects
+    this.type = "vertex3d";
+    this.x = parseFloat(x);
+    this.y = parseFloat(y);
+    this.z = parseFloat(z);
+};
